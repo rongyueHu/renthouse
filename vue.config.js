493fc4ce5432d5d8b26f035360e5e33c85137244
rongyueHu <1752333046@qq.com>
2022-06-26 20:21:14 +0800

    界面初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '租房网'
+        return args
+      })
+  }
 })
